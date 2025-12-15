b489a4240e07b577f0d216fe05c6d9b21bf337e5
orikuOlarotimi <rotimipraise07@gmail.com>
2025-12-15 23:10:45 +0100

    I learnt about prompting using system message and human message

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.messages import HumanMessage, SystemMessage
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-def generate_pet_name():
-    model = init_chat_model("google_genai:gemini-2.5-flash-lite")
-    response = model.invoke("I have a cat and i need a pet name for it please suggest me five cool names for it ")
-    return response
+def generate_pet_name(animal_type):
+    model = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=0.7,
+    )
+    system_prompt = SystemMessage(content=f"You are a creative and friendly  pet name generator. Your only task is to suggest five cool names for the pets. Do not include any conversation or explanation in your final output, just the names.")
+    human_message = HumanMessage(content=f"I have a {animal_type} and I need a pet name for it. Please suggest five cool names.")
+    response = model.invoke([system_prompt, human_message])
+    return response.content
 
 
 if __name__ == "__main__":
-    print(generate_pet_name())
+    print(generate_pet_name("rat"))
